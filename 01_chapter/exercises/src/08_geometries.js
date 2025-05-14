83d186c7782f756b00834df9ec52914b5215bec3
hiroto302 <i.am.a.hiroto@gmail.com>
2025-05-14 10:00:31 +0900

    Complete 01_Chapter in 08_geometries

---
+++ 01_chapter/exercises/src/08_geometries.js
@@ -0,0 +1,91 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+// ここでは指定した個数の三角形を表示させる
+// BufferGeometry : 大量の頂点データを扱うのに適した型
+const geometry = new THREE.BufferGeometry()
+const count = 50
+// 三角形の個数(50) * 頂点座標情報(3) * １つあたりの三角形を形作る頂点数(3)
+const positionArray = new Float32Array(count * 3 * 3)
+for (let i = 0; i < count * 3 * 3; i++)
+{
+    positionArray[i] = (Math.random() - 0.5) * 4
+}
+// BufferAttribute : 一つの頂点に対して3つの値(x,y,z)の座標を適用させるために、
+const positionAttribute = new THREE.BufferAttribute(positionArray, 3)
+geometry.setAttribute("position", positionAttribute)
+
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
