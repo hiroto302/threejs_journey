12c40dfccf80d28394ab4dfdce821808173617dc
hiroto302 <i.am.a.hiroto@gmail.com>
2025-05-16 09:59:09 +0900

    Add momo in 10_textures.js

--- 01_chapter/exercises/src/10_textures.js
+++ 01_chapter/exercises/src/10_textures.js
@@ -10,17 +10,26 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+/* TODO: Texture について
+カラーテクスチャ（Diffuse/Albedo）: オブジェクトの基本的な色や模様
+アルファテクスチャ: 透明度を制御
+ハイトテクスチャ: 表面の凹凸を表現（ディスプレイスメントマップ）
+ノーマルテクスチャ: 光の反射方向を調整して凹凸感を生み出す
+アンビエントオクルージョン: 間接光の遮蔽を表現
+メタルネス: 金属感の度合い
+ラフネス: 表面の粗さ
 
-// Textures
+テクスチャには様々なプロパティがあり、これらを調整することで見た目をカスタマイズが可能となる
+*/
 const loadingManager = new THREE.LoadingManager()
 loadingManager.onStart = () => {
-    console.log("loading started")
+    console.log("loadingManager: loading started")
 }
 loadingManager.onLoad = () => {
-    console.log("loading finished")
+    console.log("loadingManager: loading finished")
 }
 loadingManager.onProgress = () => {
-    console.log("loading progressing")
+    console.log("loadingManager: loading progressing")
 }
 loadingManager.onError = () => {
     console.log("loadingManger: loading error")
@@ -31,25 +40,26 @@ const textureLoader = new THREE.TextureLoader(loadingManager)
 // const colorTexture = textureLoader.load('/textures/checkerboard-1024x1024.png')
 // const colorTexture = textureLoader.load('/textures/checkerboard-2x2.png
 
+//NOTE: 各Loader毎にもコールバックを実装可能
 const colorTexture = textureLoader.load(
     '/textures/minecraft.png',
-    () =>
-    {
-    console.log('textureLoader: loading finished')
+    () => {
+        console.log('textureLoader: loading finished')
     },
-    () =>
-    {
-    console.log('textureLoader: loading progressing')
+    () => {
+        console.log('textureLoader: loading progressing')
     },
-    () =>
-    {
-    console.log('textureLoader: loading error')
+    () => {
+        console.log('textureLoader: loading error')
     }
 )
 
-colorTexture.colorSpace = THREE.SRGBColorSpace
-colorTexture.wrapS = THREE.MirroredRepeatWrapping
-colorTexture.wrapT = THREE.MirroredRepeatWrapping
+colorTexture.colorSpace = THREE.SRGBColorSpace      // 色空間
+colorTexture.wrapS = THREE.MirroredRepeatWrapping   // 水平方向ラッピング: 繰り返し(ミラー)
+colorTexture.wrapT = THREE.MirroredRepeatWrapping   // 垂直方向ラッピング: 繰り返し(ミラー)
+// THREE.RepeatWrapping, THREE.ClampToEdgeWrapping など他にも色々とあるよ)
+
+// オフセット(位置調整)
 // colorTexture.repeat.x = 2
 // colorTexture.repeat.y = 3
 // colorTexture.offset.x = 0.5
@@ -57,9 +67,40 @@ colorTexture.wrapT = THREE.MirroredRepeatWrapping
 // colorTexture.rotation = Math.PI * 0.25
 // colorTexture.center.x = 0.5
 // colorTexture.center.y = 0.5
+
+
+/* NOTE: Texture の各プロパティについて
+Mipmaps や Filter を活用して、オブジェクトが遠近によってTextureをどのように表示するかを設定する
+各プロパティをどのように設定すればいいか適宜考える
+
+以下、講義より抜粋
+The minification filter happens when the pixels of texture are smaller than the pixels of the render. In other words, the texture is too big for the surface, it covers.
+You can change the minification filter of the texture using the minFilter property.
+There are 6 possible values:
+
+THREE.NearestFilter
+THREE.LinearFilter
+THREE.NearestMipmapNearestFilter
+THREE.NearestMipmapLinearFilter
+THREE.LinearMipmapNearestFilter
+THREE.LinearMipmapLinearFilter
+The default is THREE.LinearMipmapLinearFilter. If you are not satisfied with how your texture looks, you should try the other filters.
+
+We won't see each one, but we will test the THREE.NearestFilter, which has a very different result:
+
+
+
+
+*/
+// ミップマップを無効化 にしてパフォーマンス向上する場合がある
 colorTexture.generateMipmaps = false
+// 縮小フィルター（テクスチャがオブジェクトより小さく表示される場合）
 colorTexture.minFilter = THREE.NearestFilter
+// 拡大フィルター（テクスチャがオブジェクトより大きく表示される場合）
 colorTexture.magFilter = THREE.NearestFilter
+
+
+
 
 
 const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
@@ -75,6 +116,34 @@ const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 console.log(geometry.attributes)
+
+/*NOTE: 作成したTextureをGeometryに適用させる
+
+MeshBasicMaterial: 最も単純で、ライトの影響を受けない。パフォーマンスは良いが、立体感に欠ける
+MeshLambertMaterial: 基本的な拡散反射のみ（ハイライトなし）
+MeshPhongMaterial: 拡散反射と鏡面反射（光沢）あり
+MeshStandardMaterial: PBRマテリアル。メタルネスとラフネスで物理的に正確な表現が可能
+MeshPhysicalMaterial: StandardMaterialの拡張版。透明度やクリアコートなど追加機能あり
+
+
+const material = new THREE.MeshStandardMaterial({
+    map: colorTexture,                           // 色テクスチャ
+    normalMap: normalTexture,                    // 法線マップ
+    metalnessMap: metalnessTexture,              // 金属度マップ
+    roughnessMap: roughnessTexture,              // 粗さマップ
+    aoMap: ambientOcclusionTexture,              // アンビエントオクルージョンマップ
+    displacementMap: heightTexture,              // 変位マップ
+    alphaMap: alphaTexture,                      // 透明度マップ
+
+    metalness: 0.5,                              // ベースの金属度（0〜1）
+    roughness: 0.5,                              // ベースの粗さ（0〜1）
+    displacementScale: 0.1,                      // 変位の強さ
+    aoMapIntensity: 1                            // AOの強さ
+});
+
+
+
+*/
 const material = new THREE.MeshBasicMaterial({ map: colorTexture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
