52232c76a43fe5206a636443e97452d406903f21
hiroto302 <i.am.a.hiroto@gmail.com>
2025-05-20 09:54:57 +0900

    18_galaxy_generator: Implement particles of cube shape
    パラメータや関数を利用して実装

--- 02_chapter/src/18_galaxy_generator.js
+++ 02_chapter/src/18_galaxy_generator.js
@@ -14,14 +14,52 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+
+/* NOTE: Galaxy
+*/
+//MEMO: こいつに DebugUI で表示したいGalaxyに関するパラメータを代入
+const parameters = {}
+parameters.count = 1000    // パーティクル数
+parameters.size = 0.02
+
+// 生成ロジック
+const generateGalaxy = () =>
+{
+    // Geometry
+    const geometry = new THREE.BufferGeometry()
+    const positions = new Float32Array(parameters.count * 3)    // パーティクルの各頂点座標
+
+    for (let i = 0; i < parameters.count; i++)
+    {
+        // 各頂点の(x,y,z)座標位置の決定
+        const i3 = i * 3
+        positions[i3 + 0] =(Math.random() - 0.5) * 3
+        positions[i3 + 1] =(Math.random() - 0.5) * 3
+        positions[i3 + 2] =(Math.random() - 0.5) * 3
+    }
+
+    geometry.setAttribute(
+        "position", new THREE.BufferAttribute(positions, 3)
+    )
+
+    // Material
+    const material = new THREE.PointsMaterial({
+        size: parameters.size,
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending
+    })
+
+    // Points
+    const points = new THREE.Points(geometry, material)
+    scene.add(points)
+
+}
+
+generateGalaxy()
+
+
+
 
 /**
  * Sizes
