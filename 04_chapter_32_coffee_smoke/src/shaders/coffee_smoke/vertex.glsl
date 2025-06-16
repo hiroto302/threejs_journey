8b698ed7520f24d9ae9154daa057faf79161407e
hiroto302 <i.am.a.hiroto@gmail.com>
2025-06-16 12:14:06 +0900

    32_coffee_smoke: Update vertex.glsl for wind animation

--- 04_chapter_32_coffee_smoke/src/shaders/coffee_smoke/vertex.glsl
+++ 04_chapter_32_coffee_smoke/src/shaders/coffee_smoke/vertex.glsl
@@ -22,9 +22,29 @@ void main()
 
   // 2. 不規則な回転
   vec3 newPosition = position;
-  float twistPerlin = texture(uPerlinTexture, vec2(0.5, uv.y * 0.2)).r;   //  perintexture の 真ん中したから上にかけての不規則なr値を取得
-  float angle = twistPerlin * 10.0;                                       //  エレベーションさせる
-  newPosition.xz = rotate2D(newPosition.xz, angle);                       //  回転
+  //  perintexture の 真ん中したから上にかけての不規則なr値を取得
+  float twistPerlin = texture(
+      uPerlinTexture,
+      vec2(0.5, uv.y * 0.2 - uTime * 0.005)
+      ).r;
+  float angle = twistPerlin * 10.0;
+  newPosition.xz = rotate2D(newPosition.xz, angle);
+
+
+  // Wind (bottom は動かず、top にかけて動くようなやつ)
+  // TODO: これ風の方向に従って動くようなものを作れるな! チャレンジしたいな！ 以下のTODOや各値を変数化すれば制御できる
+  vec2 windOffSet = vec2(
+    //NOTE: 参照する画像が白黒なので、r・g・b の値はどれも共通して 0 or 1 を返す。 なので -0.5 すれば、-0.5 ~ 0.5の値を取得可能
+    //NOTE: 0.25 や 0.75 と別の位置を参照する perlinTextureをノイズパターンが異なる値を取得したいため
+    texture(uPerlinTexture, vec2(0.25, uTime * 0.01)).r - 0.5,
+    texture(uPerlinTexture, vec2(0.75, uTime * 0.01)).r - 0.5
+  );
+  // TODO: 10.0 を -10.0 にすれば逆方向に流れる
+  // TODO: 4.0 の値を変更すれば どこから曲線を曲げるようにするか制御できる
+  windOffSet *= pow(uv.y, 4.0) * 10.0;
+
+  // 風の影響を煙に与える！
+  newPosition.xz += windOffSet;
 
 
   // Final position
