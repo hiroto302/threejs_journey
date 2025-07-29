64fbb5070a1c76c2b48be55fae83d8d7be26a210
hiroto302 <i.am.a.hiroto@gmail.com>
2025-07-29 22:18:04 +0900

    54_first_react_app: Update index.jsx and style.css for initial rendering
    - Modified index.jsx to render a more complex JSX structure including
      headings, paragraphs, and a checkbox with corresponding labels.
    - Added styles for the 'cute-paragraph' class in style.css to set
      color and background color.

--- 07_chapter_54_first_react_app/src/index.jsx
+++ 07_chapter_54_first_react_app/src/index.jsx
@@ -1,8 +1,30 @@
 import './style.css'
 import { createRoot } from 'react-dom/client'
 
+
 const root = createRoot(document.querySelector('#root'))
 
+const boolJSX = true
+const stringJSX = 'Hello World'
+const subStyle = "sub-style"
+
 root.render(
-    <h1>Hello React</h1>
+    <>
+        <h1 className='title' style={{'backgroundColor': 'floralwhite', 'color': 'coral'}}>
+            Hello { `datetime is ${Date.now()}`} <br />
+            { <em> {stringJSX} </em> } <br />
+            Are you ok? { boolJSX ? 'Yes' : 'No' }
+        </h1>
+        <p>HELLO AGAIN <strong>WORLD</strong> <br />hello world <br /></p>
+        <input type="checkbox" id="the-checkbox"/>
+        <label htmlFor="the-checkbox">Check me!</label>
+
+        <h2 style={{subStyle}}>
+            { subStyle}
+        </h2>
+
+        <h3 className='cute-paragraph' style={{ color: 'red', fontSize: '20px' }}>
+            color style
+        </h3>
+    </>
 )
