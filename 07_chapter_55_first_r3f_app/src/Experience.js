f9742dd16996a1428b43f9c205b6190961effb23
hiroto302 <i.am.a.hiroto@gmail.com>
2025-08-05 07:49:34 +0900

    55_first_r3f_app: Update Experience.js with useFrame(R3F) & useRef(React)
    - Added useRef hooks for cube and group references.
    - Implemented useFrame to rotate the cube and group continuously.
    - Updated mesh structure to include a group for better organization.
    - Changed boxGeometry to have a scale of 3 for the cube.
    - Adjusted material properties for better visual representation.

--- 07_chapter_55_first_r3f_app/src/Experience.js
+++ 07_chapter_55_first_r3f_app/src/Experience.js
@@ -1,21 +1,41 @@
-import { Wireframe } from "three/examples/jsm/Addons.js";
+import { useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
 
 export default function Experience() {
+
+    const cubeRef = useRef();
+    const groupRef = useRef();
+
+    useFrame((state, delta) =>{
+        cubeRef.current.rotation.y += delta;
+        groupRef.current.rotation.y += delta;
+    })
+
     return (
         <>
             {/* <mesh >
                 <boxGeometry args={[1, 1, 1]} />
-                <meshBasicMaterial color="red" />
+                <meshBasicMaterial color="red" />s
             </mesh> */}
             {/* <mesh>
                 <torusGeometry />
                 <meshNormalMaterial />
             </mesh> */}
-            <mesh position-x={1} rotation-y={Math.PI * 0.23} scale={1.5}>
-              {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
-              <boxGeometry />
-              {/* <meshBasicMaterial args={[{color: 'red', wireframe: true}]} /> */}
-              <meshBasicMaterial color='red' wireframe={true} />
+            <group ref={ groupRef}>
+                <mesh position-x={-2}>
+                    <sphereGeometry args={[1, 32, 32]} />
+                    <meshBasicMaterial color="orange" />
+                </mesh>
+                <mesh ref={ cubeRef } position-x={2} rotation-y={Math.PI * 0.23} scale={1.5}>
+                {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
+                    <boxGeometry scale={3}/>
+                {/* <meshBasicMaterial args={[{color: 'red', wireframe: true}]} /> */}
+                <meshBasicMaterial color='mediumpurple' wireframe={false} />
+                </mesh>
+            </group>
+            <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+                <planeGeometry />
+                <meshBasicMaterial color="greenyellow" />
             </mesh>
         </>
     );
