43dbdc9449c9384d4c37dd0206ea782515476761
hiroto302 <i.am.a.hiroto@gmail.com>
2025-08-06 12:32:45 +0900

    56_r3f_drei: Install drei & use Translate ・PivotControls
    - Added "@react-three/drei" version 10.0 to package.json and package-lock.json.
    - Refactored Experience.jsx to utilize OrbitControls, PivotControls, and TransformControls from @react-three/drei.
    - Improved mesh handling with TransformControls for better interaction.

--- 07_chapter_r3f/56_r3f_drei/src/Experience.jsx
+++ 07_chapter_r3f/56_r3f_drei/src/Experience.jsx
@@ -1,28 +1,38 @@
-import { useThree, extend } from '@react-three/fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import {
+    PivotControls,
+    TransformControls,
+    OrbitControls } from "@react-three/drei"
 
-extend({ OrbitControls })
+import { useRef } from 'react'
 
 export default function Experience()
 {
-    const { camera, gl } = useThree()
+    const cube = useRef()
 
     return <>
-
-        <orbitControls args={ [ camera, gl.domElement ] } />
+        <OrbitControls enableDamping={false} makeDefault />
 
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+        <PivotControls
+            anchor={ [ 0, 0, 0 ] }
+            depthTest={ false }
+            lineWidth={ 1 }
+            scale={ 50 }
+            fixed={ true }
+        >
+            <mesh position-x={ - 2 }>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
+        </PivotControls>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
+        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
+        <TransformControls object={ cube } mode='translate' />
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
