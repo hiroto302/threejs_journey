048f4a313004e505442414950b72a44ab4a56002
hiroto302 <i.am.a.hiroto@gmail.com>
2025-08-20 16:38:21 +0900

    59_r3f_load_models: Implement model loading with GLTFLoader and DRACOLoader
    - Updated Model.js to load a 3D model using GLTFLoader with DRACOLoader for optimized loading.
    - Added Placeholder.js to create a placeholder mesh with a box geometry and wireframe material.

--- 07_chapter_r3f/59_r3f_load_models/src/Model.js
+++ 07_chapter_r3f/59_r3f_load_models/src/Model.js
@@ -1,4 +1,25 @@
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+
 export default function Model()
 {
-  return null;
+  const model = useLoader(
+          GLTFLoader,
+          '/hamburger.glb',
+          // '/hamburger-draco.glb',
+          // './FlightHelmet/glTF/FlightHelmet.gltf',
+
+          (loader) =>
+          {
+              const dracoLoader = new DRACOLoader()
+              dracoLoader.setDecoderPath('/draco/')
+              loader.setDRACOLoader(dracoLoader)
+          }
+      )
+
+  // For hamburger model
+  return <primitive object={ model.scene } scale={ 0.35 } />
+  // For FlightHelmet model
+  // return <primitive object={ model.scene } scale={ 5 } position-y={-1} />
 }
