511c922541c7c1adad88dabce213b369d8070d0a
hiroto302 <i.am.a.hiroto@gmail.com>
2025-08-20 17:11:36 +0900

    59_r3f_load_models: Refactor model loading to use useGLTF for improved performance
    - Replaced useLoader with useGLTF for loading the hamburger model.
    - Removed commented-out code related to GLTFLoader and DRACOLoader.
    - Added preload for the hamburger model using useGLTF.

--- 07_chapter_r3f/59_r3f_load_models/src/Model.js
+++ 07_chapter_r3f/59_r3f_load_models/src/Model.js
@@ -1,25 +1,33 @@
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { useGLTF } from '@react-three/drei'
+import { use } from 'react'
 
 export default function Model()
 {
-  const model = useLoader(
-          GLTFLoader,
-          '/hamburger.glb',
-          // '/hamburger-draco.glb',
-          // './FlightHelmet/glTF/FlightHelmet.gltf',
+  // const model = useLoader(
+  //         GLTFLoader,
+  //         '/hamburger.glb',
+  //         // '/hamburger-draco.glb',
+  //         // './FlightHelmet/glTF/FlightHelmet.gltf',
 
-          (loader) =>
-          {
-              const dracoLoader = new DRACOLoader()
-              dracoLoader.setDecoderPath('/draco/')
-              loader.setDRACOLoader(dracoLoader)
-          }
-      )
+  //         (loader) =>
+  //         {
+  //             const dracoLoader = new DRACOLoader()
+  //             dracoLoader.setDecoderPath('/draco/')
+  //             loader.setDRACOLoader(dracoLoader)
+  //         }
+  //     )
 
-  // For hamburger model
+  // // For hamburger model
+  // return <primitive object={ model.scene } scale={ 0.35 } />
+  // // For FlightHelmet model
+  // // return <primitive object={ model.scene } scale={ 5 } position-y={-1} />
+
+  //NOTE: Using useGLTF from @react-three/drei for better performance
+  const model = useGLTF('/hamburger.glb')
   return <primitive object={ model.scene } scale={ 0.35 } />
-  // For FlightHelmet model
-  // return <primitive object={ model.scene } scale={ 5 } position-y={-1} />
 }
+
+useGLTF.preload('/hamburger.glb')
