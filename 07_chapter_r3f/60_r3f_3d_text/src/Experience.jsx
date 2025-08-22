d025b1bb60c8bcdf09ba8f823e02a497ea4b9d4b
hiroto302 <i.am.a.hiroto@gmail.com>
2025-08-22 11:07:34 +0900

    60_r3f_3d_text:  Text3D and matcap texture with Center
    - Imported Text3D, Center, and useMatcapTexture from '@react-three/drei'.
    - Added matcap texture functionality with a specified ID.
    - Replaced the previous mesh with a Text3D component displaying "Hello World!".
    - Configured Text3D properties such as size, height, and bevel settings.

--- 07_chapter_r3f/60_r3f_3d_text/src/Experience.jsx
+++ 07_chapter_r3f/60_r3f_3d_text/src/Experience.jsx
@@ -1,17 +1,41 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Text3D, Center, useMatcapTexture } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 
+//NOTE: https://gero3.github.io/facetype.js/ ここからフォントをダウンロード・作成できる
+
+/*NOTE: Matcapture
+https://github.com/emmelleppi/matcaps?tab=readme-ov-file MatcapTextureのサンプル, 気に入ったもの id を使用。
+idは github.com/emmelleppi/matcaps から取得される。
+今回の実装方法は、CDN(Content Delivery Network)に依存している。なので id が変わると、matcapTexture も変わる。
+なので、本番環境では、matcapTexture をローカルに保存して使用することを推奨。
+*/
 export default function Experience()
 {
+    // id: 8B892C_D4E856_475E2D_47360A
+    const [matcapTexture] = useMatcapTexture('8B892C_D4E856_475E2D_47360A', 256)
+
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        {/* Center: pivot（基準点）を幾何学的中心に移動させるイメージ */}
+        <Center position={[0, 0, 0]}>
+            <Text3D
+                    font="./fonts/helvetiker_regular.typeface.json"
+                    size={1}                   // テキストのサイズ
+                    height={0.2}               // テキストの厚み（Z軸方向の深さ）
+                    curveSegments={12}         // 曲線部分の分割数（滑らかさ）
+                    bevelEnabled={true}        // ベベル（角の丸み）を有効にするか
+                    bevelThickness={0.02}      // ベベルの厚み
+                    bevelSize={0.02}           // ベベルのサイズ
+                    bevelOffset={0}            // ベベルのオフセット
+                    bevelSegments={5}          // ベベルの分割数
+            >
+                Hello World!
+                <meshMatcapMaterial matcap={ matcapTexture } />
+            </Text3D>
+        </Center>
     </>
 }
