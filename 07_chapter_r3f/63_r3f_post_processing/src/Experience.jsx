4c6a37f6af085b1be61a7f0b5a9a509269751f70
hiroto302 <i.am.a.hiroto@gmail.com>
2025-08-23 13:04:51 +0900

    63_r3f_post_processing: Include Bloom effect
    - Added Bloom effect to the post-processing pipeline.
    - Updated comments for clarity on post-processing settings.
    - Commented out unused Vignette and Noise effects for cleaner code.

--- 07_chapter_r3f/63_r3f_post_processing/src/Experience.jsx
+++ 07_chapter_r3f/63_r3f_post_processing/src/Experience.jsx
@@ -1,34 +1,48 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { ToneMapping, EffectComposer, Vignette, Glitch, Noise } from '@react-three/postprocessing'
+import { ToneMapping, EffectComposer, Vignette, Glitch, Noise, Bloom } from '@react-three/postprocessing'
 import { ToneMappingMode, BlendFunction, GlitchMode } from 'postprocessing'
+import { texture3D } from 'three/src/nodes/TSL.js'
 
 
-/*NOTE: EffectComposer について
+/* NOTE: Post-processing について
+EffectComposer
 - mode設定は最後に行う必要があることに注意
+
+Bloom
+- luminanceThreshold: 輝度の閾値。1.0より高い値に設定すると、非常に明るい部分のみがブルーム効果の対象となる。
+    1.0以下に設定すると、より多くの部分が影響を受ける。
+- <meshStandardMaterial color={[1.5, 1, 4]} toneMapped={false}/> のように、
+    toneMappedをfalseにすると、マテリアルがトーンマッピングの影響を受けなくなる。これにより、ブルーム効果が強調される。
 */
 
 export default function Experience()
 {
     return <>
+        {/* <color args={ ['#000000'] } attach="background" /> */}
         <color args={ ['#ffffff'] } attach="background" />
 
         <EffectComposer>
-            <Vignette
+            {/* <Vignette
                 offset={ 0.3 }
                 darkness={ 0.9 }
                 blendFunction={ BlendFunction.NORMAL }
-            />
+            /> */}
             {/* <Glitch
                 delay={[ 1, 1.5 ]}
                 duration={[ 0.1, 0.3 ]}
                 strength={[ 0.02, 0.04 ]}
                 mode={ GlitchMode.CONSTANT_MIX }
             /> */}
-            <Noise
+            {/* <Noise
                 premultiply
                 blendFunction={ BlendFunction.AVERAGE }
                 opacity={ 1.7 }
+            /> */}
+            <Bloom
+                luminanceThreshold={ 1.1 }
+                mipmapBlur={ true }
+                intensity={ 1.0 }
             />
             <ToneMapping mode={ ToneMappingMode.ACES_FILMIC }/>
 
@@ -54,6 +68,13 @@ export default function Experience()
         <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+        {/* Bloom の効果を確認するために toneMapped を false に変更 */}
+        <mesh castShadow position-x={ 2 * 4 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color={[1.5, 1, 4]} toneMapped={false}/>
+            {/* <meshStandardMaterial color='orange' emissive='orange' emissiveIntensity={2} toneMapped={false}/> */}
         </mesh>
 
     </>
