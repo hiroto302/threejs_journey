44edfdb255565c559671c2c99f37a37ed896d20a
hiroto302 <i.am.a.hiroto@gmail.com>
2025-08-24 15:53:03 +0900

    65_r3f_physics: Add Physics support using @react-three/rapier
    - Updated package.json to include @react-three/rapier as a dependency.
    - Updated package-lock.json to reflect the addition of @react-three/rapier.
    - Modified Experience.jsx to implement Physics and RigidBody components for enhanced 3D interactions.

--- 07_chapter_r3f/65_r3f_physics/src/Experience.jsx
+++ 07_chapter_r3f/65_r3f_physics/src/Experience.jsx
@@ -1,5 +1,7 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { Physics, RigidBody } from '@react-three/rapier'
+
 
 export default function Experience()
 {
@@ -12,20 +14,28 @@ export default function Experience()
         <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
 
-        <mesh castShadow position={ [ - 2, 2, 0 ] }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+        <Physics debug>
+        {/* 球体 */}
+            <RigidBody>
+                <mesh castShadow position={ [ - 2, 2, 0 ] }>
+                    <sphereGeometry />
+                    <meshStandardMaterial color="orange" />
+                </mesh>
+            </RigidBody>
 
-        <mesh castShadow position={ [ 2, 2, 0 ] }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+        {/* 立方体 */}
+            <mesh castShadow position={ [ 2, 2, 0 ] }>
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
 
-        <mesh receiveShadow position-y={ - 1.25 }>
-            <boxGeometry args={ [ 10, 0.5, 10 ] } />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
+        {/* 床 */}
+            <RigidBody type='fixed'>
+                <mesh receiveShadow position-y={ - 1.25 }>
+                    <boxGeometry args={ [ 10, 0.5, 10 ] } />
+                    <meshStandardMaterial color="greenyellow" />
+                </mesh>r
+            </RigidBody>
+        </Physics>
     </>
 }
