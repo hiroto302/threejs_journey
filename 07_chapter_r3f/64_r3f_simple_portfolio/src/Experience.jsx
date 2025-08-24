0ccb1df45fe3ba4a337468fc1906a5f145422d24
hiroto302 <i.am.a.hiroto@gmail.com>
2025-08-24 14:15:01 +0900

    64_r3f_simple_portfolio: Add iframe support and lighting for MacBook Screen
    - Imported Html component from @react-three/drei to enable iframe rendering.
    - Added a rectAreaLight for improved lighting effects in the scene.
    - Wrapped the computer model in a primitive and included an iframe displaying an external webpage.
    - Updated CSS for .htmlScreen iframe to define dimensions and styling.

--- 07_chapter_r3f/64_r3f_simple_portfolio/src/Experience.jsx
+++ 07_chapter_r3f/64_r3f_simple_portfolio/src/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, PresentationControls, useGLTF, Environment, Float, ContactShadows } from '@react-three/drei'
+import { OrbitControls, PresentationControls, useGLTF, Environment, Float, ContactShadows, Html } from '@react-three/drei'
 
 /*NOTE: 使用するモデル
 CDNから直接読み込むか、ローカルに保存(./public以下)して使用してください
@@ -32,10 +32,31 @@ export default function Experience()
             snap
         >
             <Float rotationIntensity={0.4}>
+                <rectAreaLight
+                        width={ 2.5 }
+                        height={ 1.65 }
+                        intensity={ 20 }
+                        color={ '#ff6900' }
+                        rotation={ [ - 0.1, Math.PI, 0 ] }
+                        position={ [ 0, 0.55, - 1.15 ] }
+                />
                 <primitive
                     object={ computer.scene }
                     position-y={ -1.2 }
-                />
+                >
+                    <Html
+                        transform
+                        wrapperClass="htmlScreen"
+                        distanceFactor={ 1.17 }
+                        position={[0, 1.56, -1.4]}
+                        rotation-x={ -0.256 }
+                    >
+                        <iframe
+                            src="https://bruno-simon.com/html/"
+
+                        />
+                    </Html>
+                </primitive>
             </Float>
         </PresentationControls>
 
