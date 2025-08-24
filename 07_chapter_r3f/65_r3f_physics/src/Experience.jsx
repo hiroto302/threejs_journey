d426309b4777823225635c43ce8cf342b8ce6f6c
hiroto302 <i.am.a.hiroto@gmail.com>
2025-08-24 18:01:00 +0900

    65_r3f_physics:  Complicated Colliders for Cuboid & trimesh
    - Import CuboidCollider and BallCollider from @react-three/rapier
    - Add colliders to RigidBody components for better physics interactions
    - Update comments for clarity on collider types and usage

--- 07_chapter_r3f/65_r3f_physics/src/Experience.jsx
+++ 07_chapter_r3f/65_r3f_physics/src/Experience.jsx
@@ -1,6 +1,13 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { Physics, RigidBody } from '@react-three/rapier'
+import { Physics, RigidBody, CuboidCollider, BallCollider } from '@react-three/rapier'
+import { TorusGeometry } from 'three'
+
+
+/* Colliders について
+- box:　デフォルトで適用される。立方体のメッシュに対して使用。計算コストが低く、動的なオブジェクトに適している。
+- trimesh:　複雑な形状のメッシュに対して使用。計算コストが高いので、dyamic なオブジェクトにはあまり適していない。
+*/
 
 
 export default function Experience()
@@ -16,18 +23,41 @@ export default function Experience()
 
         <Physics debug>
         {/* 球体 */}
-            <RigidBody>
-                <mesh castShadow position={ [ - 2, 2, 0 ] }>
+            <RigidBody colliders="ball">
+                <mesh castShadow position={ [ 0, 4, 0 ] }>
                     <sphereGeometry />
                     <meshStandardMaterial color="orange" />
                 </mesh>
             </RigidBody>
 
-        {/* 立方体 */}
-            <mesh castShadow position={ [ 2, 2, 0 ] }>
-                <boxGeometry />
-                <meshStandardMaterial color="mediumpurple" />
-            </mesh>
+        {/* トーラス 複雑なメッシュを適用*/}
+            <RigidBody colliders="trimesh">
+                <mesh castShadow position={ [ 0, 1, 0 ] } rotation={ [ Math.PI * 0.5, 0, 0 ] }>
+                    <torusGeometry args={ [1, 0.5, 16, 32] } />
+                    <meshStandardMaterial color="hotpink" />
+                </mesh>
+            </RigidBody>
+
+        {/* トーラス カスタムコライダーを適用*/}
+            {/* <RigidBody colliders={false} position={ [ 0, 1, 0 ] } rotation={ [ Math.PI * 0.5, 0, 0 ] }>
+                <CuboidCollider args={ [1.5, 1.5, 0.5] } />
+                <mesh castShadow  >
+                    <torusGeometry args={ [1, 0.5, 16, 32] } />
+                    <meshStandardMaterial color="hotpink" />
+                </mesh>
+            </RigidBody> */}
+
+        {/* 立方体 合成*/}
+            {/* <RigidBody>
+                <mesh castShadow position={ [ 2, 2, 0 ] } >
+                    <boxGeometry args={ [3, 2, 1] } />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
+                <mesh castShadow position={ [ 2, 2, 4 ] } >
+                    <boxGeometry args={ [1, 1, 1] } />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
+            </RigidBody> */}
 
         {/* 床 */}
             <RigidBody type='fixed'>
