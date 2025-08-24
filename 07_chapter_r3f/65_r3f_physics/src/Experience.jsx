1ecce8200279bd150cdf35d96c3664ffd73f5dc7
hiroto302 <i.am.a.hiroto@gmail.com>
2025-08-24 18:17:33 +0900

    65_r3f_physics: Apply Force to Cube
    - Added a useRef hook for the cube to manage its reference.
    - Created a cubeJump function to apply impulse and torque when the cube is clicked.
    - Refactored the RigidBody for the cube to include the jump functionality.
    - Commented out the RigidBody for the torus with trimesh collider for clarity.

--- 07_chapter_r3f/65_r3f_physics/src/Experience.jsx
+++ 07_chapter_r3f/65_r3f_physics/src/Experience.jsx
@@ -2,6 +2,7 @@ import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { Physics, RigidBody, CuboidCollider, BallCollider } from '@react-three/rapier'
 import { TorusGeometry } from 'three'
+import { useRef } from 'react'
 
 
 /* Colliders について
@@ -12,6 +13,14 @@ import { TorusGeometry } from 'three'
 
 export default function Experience()
 {
+    const cube = useRef()
+    const cubeJump = () =>
+    {
+        // console.log(cube.current)
+        cube.current.applyImpulse({ x: 0, y: 5, z: 0 })
+        cube.current.applyTorqueImpulse({ x: 0, y: 1, z: 0 })
+    }
+
     return <>
 
         <Perf position="top-left" />
@@ -31,12 +40,12 @@ export default function Experience()
             </RigidBody>
 
         {/* トーラス 複雑なメッシュを適用*/}
-            <RigidBody colliders="trimesh">
+            {/* <RigidBody colliders="trimesh">
                 <mesh castShadow position={ [ 0, 1, 0 ] } rotation={ [ Math.PI * 0.5, 0, 0 ] }>
                     <torusGeometry args={ [1, 0.5, 16, 32] } />
                     <meshStandardMaterial color="hotpink" />
                 </mesh>
-            </RigidBody>
+            </RigidBody> */}
 
         {/* トーラス カスタムコライダーを適用*/}
             {/* <RigidBody colliders={false} position={ [ 0, 1, 0 ] } rotation={ [ Math.PI * 0.5, 0, 0 ] }>
@@ -58,6 +67,14 @@ export default function Experience()
                     <meshStandardMaterial color="mediumpurple" />
                 </mesh>
             </RigidBody> */}
+
+        {/* 立方体 ジャンプ */}
+            <RigidBody ref={ cube } position={ [ 1.5, 2, 0 ] }>
+                <mesh castShadow onClick={ cubeJump } >
+                    <boxGeometry args={ [1, 1, 1] } />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
+            </RigidBody>
 
         {/* 床 */}
             <RigidBody type='fixed'>
