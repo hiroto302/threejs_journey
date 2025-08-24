c57069c2a23f8c69780fdc04f989c01a4ac2279c
hiroto302 <i.am.a.hiroto@gmail.com>
2025-08-24 11:39:50 +0900

    64_r3f_simple_portfolio:  Include Macbook 3D model and environment
    - Import useGLTF, Environment, and Float from '@react-three/drei'
    - Add a note for model usage and CDN links
    - Load MacBook Pro 3D model using useGLTF
    - Set environment preset to "city" and background color
    - Replace placeholder mesh with a Float component for the 3D model

--- 07_chapter_r3f/64_r3f_simple_portfolio/src/Experience.jsx
+++ 07_chapter_r3f/64_r3f_simple_portfolio/src/Experience.jsx
@@ -1,15 +1,35 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useGLTF, Environment, Float } from '@react-three/drei'
+
+/*NOTE: 使用するモデル
+CDNから直接読み込むか、ローカルに保存(./public以下)して使用してください
+もしくは、React Three Fiber > Copy JSX Scene Graph などで取得したコードを使用してください
+
+- MacBook Pro 3Dモデル
+https://market.pmnd.rs/model/macbook
+    - CDN
+    https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf
+
+-  PMNDRs market がダウンしている場合は、以下のURLを使用してください
+const computer = useGLTF('https://threejs-journey.com/resources/models/macbook_model.gltf')
+*/
 
 export default function Experience()
 {
+    const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
+    console.log(computer)
+
     return <>
+        <Environment preset="city" />
+        <color attach="background" args={['#241a1a']} />
 
         <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+
+        <Float rotationIntensity={0.4}>
+            <primitive
+                object={ computer.scene }
+                position-y={ -1.2 }
+            />
+        </Float>
 
     </>
 }
