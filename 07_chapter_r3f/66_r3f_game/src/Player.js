b73c0b70b8dc21943297fe5a1aeaf9f6c5e13e02
hiroto302 <i.am.a.hiroto@gmail.com>
2025-08-26 08:27:44 +0900

    66_r3f_game: Add Camera Animation
    - Set Physics debug mode to false in Experience.jsx for better performance.
    - Import useState and THREE in Player.js to manage state and handle camera positioning.
    - Implement camera follow logic in Player.js to enhance player experience.

--- 07_chapter_r3f/66_r3f_game/src/Player.js
+++ 07_chapter_r3f/66_r3f_game/src/Player.js
@@ -1,7 +1,8 @@
 import { useRapier , RigidBody } from "@react-three/rapier"
 import { useFrame } from "@react-three/fiber"
 import { useKeyboardControls } from "@react-three/drei"
-import { useRef, useEffect } from "react"
+import { useRef, useEffect, useState } from "react"
+import * as THREE from "three"
 
 export default function Player()
 {
@@ -57,6 +58,8 @@ export default function Player()
   }, [ ])
 
   useFrame((state, delta) => {
+
+    // Controls
     const { forward, backward, leftward, rightward } = getKeys()
 
     const impulse = { x: 0 , y: 0, z: 0 }
@@ -84,6 +87,20 @@ export default function Player()
 
     body.current.applyImpulse(impulse)
     body.current.applyTorqueImpulse(torque)
+
+    // Camera
+    const bodyPosition = body.current.translation()
+    const cameraPosition = new THREE.Vector3()
+    cameraPosition.copy(bodyPosition)
+    cameraPosition.z += 2.24
+    cameraPosition.y += 0.65
+
+    const cameraTarget = new THREE.Vector3()
+    cameraTarget.copy(bodyPosition)
+    cameraTarget.y += 0.25
+
+    state.camera.position.lerp(cameraPosition, 5 * delta)
+    state.camera.lookAt(cameraTarget)
   })
 
   return <>
