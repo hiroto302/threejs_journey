04c3157858b8bf8c19fc17a6381253ac7274b5a3
hiroto302 <i.am.a.hiroto@gmail.com>
2025-08-26 08:08:30 +0900

    66_r3f_game: Refactor Player component to improve jump mechanics
    - Import useRapier for better physics handling.
    - Implement raycasting to prevent double jumping.
    - Update jump logic to apply impulse only if the player is grounded.
    - Enhance useEffect to manage jump state subscription more clearly.

--- 07_chapter_r3f/66_r3f_game/src/Player.js
+++ 07_chapter_r3f/66_r3f_game/src/Player.js
@@ -1,36 +1,59 @@
-import { RigidBody } from "@react-three/rapier"
+import { useRapier , RigidBody } from "@react-three/rapier"
 import { useFrame } from "@react-three/fiber"
 import { useKeyboardControls } from "@react-three/drei"
 import { useRef, useEffect } from "react"
 
 export default function Player()
 {
+  console.log('Player Reloaded')
   const body = useRef()
 
   // const { forward, backward, leftward, rightward, jump } = useKeyboardControls()
   const [ subscribeKeys, getKeys ] = useKeyboardControls()
 
+  const { rapier, world } = useRapier()
+  // Point: world.raw() gives access to the underlying Rapier physics world だったが、現在は、world自体がrawになっている
+  // なので、world.raw()ではなく、worldで良い
+
   const jump = () => {
-    body.current.applyImpulse({ x: 0, y: 0.5, z: 0 })
+    // Pint: ダブルジャンプ防止
+    const origin = body.current.translation()
+    origin.y -= 0.31
+    const direction = { x: 0, y: -1, z: 0 }
+    const ray = new rapier.Ray(origin, direction)
+    const hit = world.castRay(ray, 10, true) // trueにすることで、最初にヒットしたオブジェクトだけを取得する
+    // console.log('hit', hit.timeOfImpact)
+
+    if(hit.timeOfImpact < 0.15)
+    {
+      body.current.applyImpulse({ x: 0, y: 0.5, z: 0 })
+    }
   }
 
   useEffect(() =>
   {
-    subscribeKeys(
-      // jumpの状態が変化したときに呼ばれる
+    console.log('useEffect')
+    const unsubscribeJump = subscribeKeys(
+      // 第一引数は、stateを受け取る関数で、stateはuseKeyboardControlsの中の状態を表す
+      // ここでは、jumpの状態を監視する
       (state) =>
       {
         return state.jump
       },
-      // jumpが押されたときに呼ばれる
+      // 第二引数は、状態が変化したときに呼び出される関数で、valueは新しい状態の値を表す
+      // ここでは、jumpが押されたとき(状態が変化した時)に呼び出される
       (value) =>
       {
-        console.log('jump', value)
+        // console.log('jump')
         if(value) {
           jump()
         }
       }
     )
+
+    return () => {
+      unsubscribeJump()
+    }
   }, [ ])
 
   useFrame((state, delta) => {
