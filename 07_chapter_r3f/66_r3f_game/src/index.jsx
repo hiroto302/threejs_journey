69bc94378bcf51753de5ed32e1aa393f727daf96
hiroto302 <i.am.a.hiroto@gmail.com>
2025-08-27 23:46:04 +0900

    66_r3f_game: Add Interface component for game controls and UI
    - Created a new Interface component to manage game controls and display UI elements.
    - Integrated keyboard controls for movement and actions.
    - Added visual elements for time display, restart button, and control keys.
    - Updated index.jsx to include the new Interface component.
    - Enhanced style.css with styles for the new interface elements.

--- 07_chapter_r3f/66_r3f_game/src/index.jsx
+++ 07_chapter_r3f/66_r3f_game/src/index.jsx
@@ -4,6 +4,7 @@ import { Canvas } from '@react-three/fiber'
 import { KeyboardControls } from '@react-three/drei'
 
 import Experience from './Experience.jsx'
+import Interface from './interface.js'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
@@ -28,6 +29,6 @@ root.render(
             >
                 <Experience />
         </Canvas>
+        <Interface />
     </KeyboardControls>
-    
 )
