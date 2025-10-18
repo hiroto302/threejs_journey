daa0284b1b139943eb8476444b3799f681388ee3
hiroto302 <i.am.a.hiroto@gmail.com>
2025-10-18 22:56:43 +0900

    34_fireworks: Add texture support for fireworks particles
    - Load multiple particle textures for fireworks.
    - Update createFirework function to accept a texture parameter.
    - Adjust fragment shader to utilize the new texture uniform for rendering.

--- 04_chapter_shaders/34_fireworks/src/script.js
+++ 04_chapter_shaders/34_fireworks/src/script.js
@@ -76,7 +76,18 @@ renderer.setPixelRatio(sizes.pixelRatio)
 /**
  * Fireworks
  */
-const createFirework = (count, position, size) =>
+const textures = [
+    textureLoader.load('./particles/1.png'),
+    textureLoader.load('./particles/2.png'),
+    textureLoader.load('./particles/3.png'),
+    textureLoader.load('./particles/4.png'),
+    textureLoader.load('./particles/5.png'),
+    textureLoader.load('./particles/6.png'),
+    textureLoader.load('./particles/7.png'),
+    textureLoader.load('./particles/8.png'),
+]
+
+const createFirework = (count, position, size, texture) =>
 {
     const positionsArray = new Float32Array(count * 3)
     for (let i = 0; i < count; i++)
@@ -90,6 +101,7 @@ const createFirework = (count, position, size) =>
         geometry.setAttribute('position', new THREE.Float32BufferAttribute(positionsArray, 3))
 
         // Material
+        texture.flipY = false
         const material = new THREE.ShaderMaterial({
             vertexShader: fireworkVertexShader,
             fragmentShader: fireworkFragmentShader,
@@ -97,8 +109,14 @@ const createFirework = (count, position, size) =>
             {
                 uSize: new THREE.Uniform(size),
                 //NOTE: adjust size based on your screen resolution
-                uResolution: new THREE.Uniform(sizes.resolution)
-            }
+                uResolution: new THREE.Uniform(sizes.resolution),
+                uTexture: new THREE.Uniform(texture)
+            },
+            //NOTE: パーティクルの描画設定
+            transparent: true,
+            depthWrite: false,
+            // blending: THREE.AdditiveBlending
+
         })
 
         // Points
@@ -111,7 +129,8 @@ const createFirework = (count, position, size) =>
 createFirework(
     100,                    // count
     new THREE.Vector3(),    // position
-    0.25                    // size
+    0.25,                   // size
+    textures[7]
 )
 
 /**
--- 04_chapter_shaders/34_fireworks/src/shaders/firework/fragment.glsl
+++ 04_chapter_shaders/34_fireworks/src/shaders/firework/fragment.glsl
@@ -1,7 +1,14 @@
+uniform sampler2D uTexture;
+
 void main()
 {
+  vec4 textureColor = texture2D(uTexture, gl_PointCoord);
+  float textureAlpha = textureColor.r;
+
   // Final color
-  gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+  // gl_FragColor = vec4(gl_PointCoord, 1.0, 1.0);
+  // gl_FragColor = vec4(textureColor.rgb, 1.0);
+  gl_FragColor = vec4(textureColor.rgb, textureAlpha);
 
   #include <tonemapping_fragment>
   #include <colorspace_fragment>
