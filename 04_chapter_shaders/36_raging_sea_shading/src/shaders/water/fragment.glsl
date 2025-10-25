82d973f21ecb6d8013ad8698113d13906af6f47e
hiroto302 <i.am.a.hiroto@gmail.com>
2025-10-25 13:48:57 +0900

    36_raging_sea_shading: apply directional color to base color
    - In fragment.glsl:
      - Added light calculations using directional light.
      - Updated the final color output to use normal instead of color.
    
    - In vertex.glsl:
      - Added varying variables for normal and position.
      - Updated the varying assignments to pass normal and position to the fragment shader.

--- 04_chapter_shaders/36_raging_sea_shading/src/shaders/water/fragment.glsl
+++ 04_chapter_shaders/36_raging_sea_shading/src/shaders/water/fragment.glsl
@@ -4,16 +4,39 @@ uniform float uColorOffset;
 uniform float uColorMultiplier;
 
 varying float vElevation;
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+#include ../includes/directionalLight.glsl
 
 void main()
 {
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
+    vec3 normal = normalize(vNormal);
+
     // Base color
     float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
     mixStrength = smoothstep(0.0, 1.0, mixStrength);
     vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
 
+    // Light
+    vec3 light = vec3(0.0);
+    light += directionalLight(
+        vec3(1.0),             // lightColor
+        1.0,                   // lightIntensity
+        normal,                // normal
+        vec3(-1.0, 0.5, 0.0),  // lightDirection
+        viewDirection,         // viewDirection
+        30.0                   // specularPower
+    );
+
+    // Apply light
+    color *= light;
+
     // Final color
-    gl_FragColor = vec4(color, 1.0);
+    // gl_FragColor = vec4(color, 1.0);
+    // gl_FragColor = vec4(vPosition, 1.0);
+    gl_FragColor = vec4(vNormal, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
