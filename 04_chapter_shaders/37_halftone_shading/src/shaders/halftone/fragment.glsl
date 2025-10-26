2bd116ff96e3b06e28baeb944b1d0678af582423
hiroto302 <i.am.a.hiroto@gmail.com>
2025-10-27 08:42:46 +0900

    37_halftone_shading: Add Radius & Intensity
    - Increased repetitions from 30.0 to 50.0 for finer halftone grid.
    - Added direction, low, and high variables for intensity calculations.
    - Modified point distance calculation to use intensity for better visual results.

--- 04_chapter_shaders/37_halftone_shading/src/shaders/halftone/fragment.glsl
+++ 04_chapter_shaders/37_halftone_shading/src/shaders/halftone/fragment.glsl
@@ -33,18 +33,28 @@ void main()
 
     // Halftone
     // Grid
-    float repetitions = 30.0;
+    float repetitions = 50.0;
+    vec3 direction = vec3(0.0, -1.0, 0.0);
+    float low = -0.8;
+    float high = 1.5;
+
     vec2 uv = gl_FragCoord.xy / uResolution.y;
     uv = mod(uv * repetitions, 1.0);
+
+    // Radius & Intensity
+    float intensity = dot(normal, direction);
+    intensity = smoothstep(low, high, intensity);
+
     // Point distance from center
     float point = distance(uv, vec2(0.5));
-    point = 1.0 - step(0.5, point);
+    point = 1.0 - step(0.5 * intensity, point);
 
 
     // Final color
     // gl_FragColor = vec4(color, 1.0);
     // gl_FragColor = vec4(uv, 1.0, 1.0);
     gl_FragColor = vec4(point, point, point, 1.0);
+    // gl_FragColor = vec4(intensity, intensity, intensity, 1.0);
 
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
