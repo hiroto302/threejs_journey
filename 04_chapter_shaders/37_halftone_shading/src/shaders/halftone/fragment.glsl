49e5ba9e1e609687e1673fc251edd6b215726593
hiroto302 <i.am.a.hiroto@gmail.com>
2025-10-27 09:05:57 +0900

    37_halftone_shading: Implement halftone function for color mixing
    - Added a new `halftone` function to apply halftone effect based on color, repetitions, direction, normal, and intensity.
    - Updated the `main` function to utilize the new `halftone` function for final color calculation.
    - Removed commented-out code for clarity.

--- 04_chapter_shaders/37_halftone_shading/src/shaders/halftone/fragment.glsl
+++ 04_chapter_shaders/37_halftone_shading/src/shaders/halftone/fragment.glsl
@@ -8,6 +8,32 @@ varying vec3 vPosition;
 #include ../includes/directionalLight.glsl
 #include ../includes/pointLight.glsl
 
+vec3 halftone(
+    vec3 color,
+    float repetitions,
+    vec3 direction,
+    vec3 normal,
+    float low,
+    float high,
+    vec3 pointColor
+)
+{
+    // Grid
+    vec2 uv = gl_FragCoord.xy / uResolution.y;
+    uv = mod(uv * repetitions, 1.0);
+
+    // Radius & Intensity
+    float intensity = dot(normal, direction);
+    intensity = smoothstep(low, high, intensity);
+
+    // Point distance from center
+    float point = distance(uv, vec2(0.5));
+    point = 1.0 - step(0.5 * intensity, point);
+
+    // Apply halftone
+    return mix(color, pointColor, point);
+}
+
 void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
@@ -31,13 +57,15 @@ void main()
     color *= light;
 
 
-    // Halftone
-    // Grid
+// Halftone
+    // Parameters
     float repetitions = 50.0;
     vec3 direction = vec3(0.0, -1.0, 0.0);
     float low = -0.8;
     float high = 1.5;
+    vec3 pointColor = vec3(1.0, 0.0, 0.0);
 
+    // Grid
     vec2 uv = gl_FragCoord.xy / uResolution.y;
     uv = mod(uv * repetitions, 1.0);
 
@@ -49,12 +77,28 @@ void main()
     float point = distance(uv, vec2(0.5));
     point = 1.0 - step(0.5 * intensity, point);
 
+    // Apply halftone
+    // color = mix(color, pointColor, point);
 
     // Final color
     // gl_FragColor = vec4(color, 1.0);
     // gl_FragColor = vec4(uv, 1.0, 1.0);
-    gl_FragColor = vec4(point, point, point, 1.0);
+    // gl_FragColor = vec4(point, point, point, 1.0);
     // gl_FragColor = vec4(intensity, intensity, intensity, 1.0);
+
+
+    // use func halftone
+    color = halftone(
+        color,
+        30.0,
+        vec3(0.0, -1.0, 0.0),
+        normal,
+        - 0.8,
+        1.5,
+        vec3(1.0, 0.0, 0.0)
+    );
+
+    gl_FragColor = vec4(color, 1.0);
 
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
