002c4fb7b46543f797a47a439558793d192540d0
hiroto302 <i.am.a.hiroto@gmail.com>
2025-11-01 00:45:03 +0900

    38_earth: Implement day/night texture blending
    - Initialize color to vec3(0.0) instead of using vUv.
    - Add sun orientation calculation and day/night texture mixing logic.
    - Use smoothstep for blending between day and night colors based on sun orientation.

--- 04_chapter_shaders/38_earth/src/shaders/earth/fragment.glsl
+++ 04_chapter_shaders/38_earth/src/shaders/earth/fragment.glsl
@@ -10,7 +10,18 @@ void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
-    vec3 color = vec3(vUv, 1.0);
+    vec3 color = vec3(0.0);
+
+    // Sun orientation
+    vec3 sunDirection = normalize(vec3(0.0, 0.0, 1.0));
+    float sunOrientation = dot(sunDirection, normal);
+
+    // Day・Night textures of color
+    float dayMix = smoothstep(-0.25, 0.5, sunOrientation);
+    vec3 dayColor = texture(uDayTexture, vUv).rgb;
+    vec3 nightColor = texture(uNightTexture, vUv).rgb;
+    color = mix(dayColor, nightColor, dayMix);
+    // color = mix(nightColor, dayColor, smoothstep(-0.1, 0.1, sunOrientation));
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
