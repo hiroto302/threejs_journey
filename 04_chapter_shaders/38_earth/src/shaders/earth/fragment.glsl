fa1df0097e009ea64f9f9cf1b63dacd7e3968333
hiroto302 <i.am.a.hiroto@gmail.com>
2025-11-04 00:17:21 +0900

    38_earth: Update specular clouds texture handling in fragment shader
    - Changed uniform sampler2D from uSpecularTexture to uSpecularCloudsTexture.
    - Adjusted color mixing logic to incorporate specular clouds based on density.
    - Enhanced day/night texture blending with cloud effects for improved visual quality.

--- 04_chapter_shaders/38_earth/src/shaders/earth/fragment.glsl
+++ 04_chapter_shaders/38_earth/src/shaders/earth/fragment.glsl
@@ -1,6 +1,6 @@
 uniform sampler2D uDayTexture;
 uniform sampler2D uNightTexture;
-uniform sampler2D uSpecularTexture;
+uniform sampler2D uSpecularCloudsTexture;
 uniform vec3 uSunDirection;
 
 
@@ -15,15 +15,21 @@ void main()
     vec3 color = vec3(0.0);
 
     // Sun orientation
-   
     float sunOrientation = dot(uSunDirection, normal);
 
     // Day・Night textures of color
     float dayMix = smoothstep(-0.25, 0.5, sunOrientation);
     vec3 dayColor = texture(uDayTexture, vUv).rgb;
     vec3 nightColor = texture(uNightTexture, vUv).rgb;
-    color = mix(dayColor, nightColor, dayMix);
-    // color = mix(nightColor, dayColor, smoothstep(-0.1, 0.1, sunOrientation));
+    color = mix(nightColor, dayColor, dayMix);
+
+    // Specular clouds texture (rチャンネル: 鏡面反射, gチャンネル: 雲の密度)
+    vec2 specularCloudsColor = texture(uSpecularCloudsTexture, vUv).rg;
+
+    // Clouds
+    float cloudMix = smoothstep(0.5, 1.0, specularCloudsColor.g);
+    cloudMix *= dayMix;
+    color = mix(color, vec3(1.0), cloudMix);
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
