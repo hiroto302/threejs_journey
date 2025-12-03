d85b9cae5c997f84f65eafd1391ad7e97db43f74
hiroto302 <i.am.a.hiroto@gmail.com>
2025-12-03 23:59:27 +0900

    39_particles_cursor_anim: Update particle displacement logic and enhance comments
    - Added detailed comments to explain the particle force definitions and displacement calculations.
    - Improved clarity on how displacement direction is calculated using cosine and sine functions.
    - Adjusted the scaling of displacement to enhance particle movement dynamics.

--- 04_chapter_shaders/39_particles_cursor_anim/src/shaders/particles/vertex.glsl
+++ 04_chapter_shaders/39_particles_cursor_anim/src/shaders/particles/vertex.glsl
@@ -1,3 +1,20 @@
+/* パーティクルにどのような力を与えるか定義
+    * smoothstep(0.1, 0.3, displacementIntensity)
+        0.1 以下の値を 0 に、0.3 以上の値を 1 に変換
+        0.1 から 0.3 の間の値を 0 から 1 の間に線形補間
+
+        0.3の値を1にすることで、パーティクルがより早く最大変位に達するように調整。
+        動きがよりダイナミックで視覚的に興味深くなる。値を変更して遊んでみよう。
+
+    * Displacement direction
+        cos(aAngle), sin(aAngle) を使って、ランダムな方向にパーティクルを動かす。
+        これにより、パーティクルが均一に広がり、自然な動きが生まれる。
+        円周上の方向を作ります。これでパーティクルは中心から放射状に散らばります。
+
+    
+
+*/
+
 uniform vec2 uResolution;
 uniform sampler2D uPictureTexture;
 uniform sampler2D uDisplacementTexture;
@@ -14,16 +31,21 @@ void main()
     float displacementIntensity = texture(uDisplacementTexture, uv).r;
     displacementIntensity = smoothstep(0.1, 0.3, displacementIntensity);
 
+    // Calculate displacement direction (パーティクルが動く方向を決定)
     vec3 displacement = vec3(
-        cos(aAngle) * 0.2,
-        sin(aAngle) * 0.2,
-        1.0
+        cos(aAngle) * 0.2,      // x軸方向の変位
+        sin(aAngle) * 0.2,      // y軸方向の変位
+        1.0                     // z軸方向の変位
     );
+    // Normalize and scale displacement
     displacement = normalize(displacement);
+    // Apply intensity of movement mouse
     displacement *= displacementIntensity;
+    // 移動距離の倍率を1.0から3.0に変更して、より大きく動くように調整
     displacement *= 3.0;
+    // Apply randomness intensity
     displacement *= aIntensity;
-
+    // Update new position
     newPosition += displacement;
 
     // Final position
