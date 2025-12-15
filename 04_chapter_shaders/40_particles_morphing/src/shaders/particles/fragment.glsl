0a0ef42c35a9ebec1eeaaddee5f2e553c385b729
hiroto302 <i.am.a.hiroto@gmail.com>
2025-12-15 23:59:35 +0900

    40_particles_morphing: Refine alpha calculation for better visibility
    - Added comments to clarify the purpose of alpha computation based on distance to center.
    - Enhanced readability of the shader code by organizing comments.

--- 04_chapter_shaders/40_particles_morphing/src/shaders/particles/fragment.glsl
+++ 04_chapter_shaders/40_particles_morphing/src/shaders/particles/fragment.glsl
@@ -1,9 +1,12 @@
 void main()
 {
+    // Compute alpha based on distance to center
     vec2 uv = gl_PointCoord;
     float distanceToCenter = length(uv - 0.5);
+    // Adjusted alpha calculation for better visibility
     float alpha = 0.05 / distanceToCenter - 0.1;
 
+    // Final color
     gl_FragColor = vec4(1.0, 1.0, 1.0, alpha);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
